577c0abdd4086318200c84751f1e9857d6d321e9
Paulo Faria <paulo.faria.rl@gmail.com>
2017-06-09 19:45:18 -0300

    install libdill when not installed

--- install-libdill.sh
+++ install-libdill.sh
@@ -7,7 +7,7 @@ UNAME=`uname`;
 if [[ $UNAME == "Darwin" ]]; then
     brew install zewo/tap/libdill
 elif [[ $UNAME == "Linux" ]]; then
-	if (( EUID == 0 )); then
+	if [ $(dpkg-query -W -f='${Status}' libdill 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
 		echo "deb [trusted=yes] http://apt.zewo.io ./" | sudo tee -a /etc/apt/sources.list
     	sudo apt-get update
     	sudo apt-get install libdill
