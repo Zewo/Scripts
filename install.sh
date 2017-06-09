68767ce9181bd6b714b9397240360905017cd0d5
Paulo Faria <paulo.faria.rl@gmail.com>
2017-06-09 19:30:52 -0300

    install clang if not installed

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ fi
 echo "🖥 Operating System: $OS";
 
 if [[ $OS != "macOS" ]]; then
-    if (( EUID == 0 )); then
+    if ! hash clang 2> /dev/null; then
         sudo apt-get update
         sudo apt-get install clang-3.6 libicu-dev zewo
         sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.6 100
