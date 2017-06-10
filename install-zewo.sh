bd9fee0eee03e1fd3fa18da21d156fd27acaac1f
Paulo Faria <paulo.faria.rl@gmail.com>
2017-06-10 14:46:27 -0300

    add install-zewo.sh

---
+++ install-zewo.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+eval "$(curl -sL http://sh.zewo.io/install.sh)"
+
+UNAME=`uname`;
+
+if [[ $UNAME == "Darwin" ]]; then
+    brew install zewo/tap/zewo
+elif [[ $UNAME == "Linux" ]]; then
+	if [ $(dpkg-query -W -f='${Status}' libdill 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+		echo "deb [trusted=yes] http://apt.zewo.io ./" | sudo tee -a /etc/apt/sources.list
+    	sudo apt-get update
+    	sudo apt-get install zewo
+	fi
+else
+    echo "❌ Unsupported Operating System: $UNAME";
+    exit 1; 
+fi
