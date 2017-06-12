bebc92cfeca19a0f0a7e6bce9817da77097581a6
Paulo Faria <paulo.faria.rl@gmail.com>
2017-06-12 16:19:43 -0300

    echo installing zewo messages

--- install-zewo.sh
+++ install-zewo.sh
@@ -5,7 +5,10 @@ eval "$(curl -sL http://sh.zewo.io/install.sh)"
 UNAME=`uname`;
 
 if [[ $UNAME == "Darwin" ]]; then
+	echo "🔥 Installing Zewo";
     brew install zewo/tap/zewo
+    echo "💥 Succesfully installed Zewo";
+    exit 0; 
 elif [[ $UNAME == "Linux" ]]; then
 	if [ $(dpkg-query -W -f='${Status}' libdill 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
 		echo "deb [trusted=yes] http://apt.zewo.io ./" | sudo tee -a /etc/apt/sources.list
